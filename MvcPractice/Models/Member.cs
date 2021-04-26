15b4ab3cf394df87a1dbcaced2ad5e8843fa7a76
lena <islenaliny@gmail.com>
2021-04-26 15:01:55 +0800

    Initial commit

---
+++ MvcPractice/Models/Member.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcPractice.Models
+{
+    public class Member
+    {
+        public int Id { get; set; }
+
+
+    }
+}
